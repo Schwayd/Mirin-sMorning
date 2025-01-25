ae105648f4889cae4e77a625872b2dd3beaadefb
Schwayd <Zschack@hotmail.com>
2025-01-25 22:16:22 +0000

    Rotation on grinder handle
    Added ability to drag handle around with mouse input

--- Assets/GrinderMouseRotate.cs
+++ Assets/GrinderMouseRotate.cs
@@ -1,22 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class GrinderMouseRotate : MonoBehaviour
+public class GrinderHandle : MonoBehaviour, IDragHandler
 {
+    private RectTransform rectTransform;
+    private Vector2 centerPoint;
 
-    public float rotationSpeed;
-
-    private void OnMouseDrag()
+    void Start()
     {
-        float rotY = Input.GetAxis("Mouse Y") * rotationSpeed;
-        float rotX = Input.GetAxis("Mouse X") * rotationSpeed;
-
-        transform.Rotate(Vector3.down, rotX, Space.World);
-        transform.Rotate(Vector3.right, rotY, Space.World);
+        rectTransform = GetComponent<RectTransform>();
+        // Calculate the center of the handle in screen space
+        centerPoint = RectTransformUtility.WorldToScreenPoint(null, rectTransform.position);
     }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Calculate the direction vector from the center to the mouse position
+        Vector2 direction = eventData.position - centerPoint;
 
+        // Calculate the angle in degrees (relative to the upward vector)
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
+        // Correct the angle to match the initial handle orientation
+        angle -= 90; // Adjust this based on your handle's initial rotation in the Scene
+
+        // Apply the rotation to the handle
+        rectTransform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 }
-
