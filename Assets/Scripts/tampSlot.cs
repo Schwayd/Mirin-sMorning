f72eeadee994c2ccfaa6d9969db59ae8b091b1ee
Schwayd <Zschack@hotmail.com>
2025-02-04 17:38:19 +0000

    Added images

--- Assets/Scripts/tampSlot.cs
+++ Assets/Scripts/tampSlot.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class tampSlot : MonoBehaviour, IDropHandler
 {
+    
+
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
         if (eventData != null)
         {
+            //gets the object attached to the pointer dragging and snaps to the location of the tampConnecter eobject
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            
         }
     }
-
-
-
 }
