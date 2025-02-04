bd4896c2f678b6fc9a842b6327670324b7227f48
Schwayd <Zschack@hotmail.com>
2025-02-04 17:05:26 +0000

    Portafilter drags and connects
    Simple script to drag the portafilter around and the screen and a snap point where it connects to the expresso machine

--- Assets/brewMechanic.cs
+++ Assets/brewMechanic.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class brewMechanic : MonoBehaviour, IPointerDownHandler
+public class brewMechanic : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler
 {
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnpointerDown");
     }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+    
+    public void OnEndDrag(PointerEventData data)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
 
+    public void OnBeginDrag(PointerEventData Data)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = .8f;
+    }
 
+    public void OnDrop(PointerEventData Data)
+    {
+
+    }
 }
