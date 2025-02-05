5303b2fcb176c950044a6cac6a3dd62e649c9023
schwayd <zschack@hotmail.com>
2025-02-05 14:31:56 +0000

    Integerating Articy importer part 1
    Articy Importer has been set up in unity. A new scene has alos been setup with the diaogue menu. The dialogue manager has been scripted to show the first line of dialogue.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Articy.Unity;
+using Articy.Unity.Interfaces;
+
+
+public class DialogueManager : MonoBehaviour, IArticyFlowPlayerCallbacks
+{
+    [Header("UI")]
+    // Reference to Dialog UI
+    [SerializeField]
+    GameObject dialogueWidget;
+    // Reference to dialogue text
+    [SerializeField]
+    Text dialogueText;
+    // Reference to speaker
+    [SerializeField]
+    Text dialogueSpeaker;
+
+    // To check if we are currently showing the dialog ui interface
+    public bool DialogueActive { get; set; }
+
+
+    private ArticyFlowPlayer flowPlayer; 
+    void Start()
+    {
+        //Triggers the flowplayer to traverse through the script
+        flowPlayer = GetComponent<ArticyFlowPlayer>();
+    }
+    
+    public void StartDialogue(string aDialogueLine, string aSpeaker)
+    {
+        //begins the dialogue
+        DialogueActive = true;
+        dialogueWidget.SetActive(DialogueActive);       
+        
+        dialogueText.text = aDialogueLine;
+        dialogueSpeaker.text = aSpeaker;
+    }
+
+    public void CloseDialogueBox()
+    {
+        //Hides the dialogue UI 
+        DialogueActive = false;
+        dialogueWidget.SetActive(DialogueActive);        
+    }
+
+    //These two methods below extracts information from the node it paused on and tell the flow player what to do next
+    public void OnFlowPlayerPaused(IFlowObject aObject)
+    {
+        dialogueText.text = string.Empty;
+
+        var objectWithText = aObject as IObjectWithLocalizableText;
+        if(objectWithText != null)
+        {
+            dialogueText.text = objectWithText.Text;
+        }
+    }
+
+    public void OnBranchesUpdated(IList<Branch> aBranches)
+    {
+
+    }
+}
