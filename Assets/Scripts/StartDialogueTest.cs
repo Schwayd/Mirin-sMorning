b7b8a893aacc1d3da2c202ceb788de1b9ce7fe5c
Schwayd <Zschack@hotmail.com>
2025-02-07 23:13:43 +0000

    Brew mechanics + coffee shop
    Brew mechanics will show cutscenes placeholder when you press brew button. Coffee shop is imported and got a 2d sprite behind the counter

---
+++ Assets/Scripts/StartDialogueTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Articy.Unity;
+using Articy.Unity.Interfaces;
+
+
+
+public class StartDialogueTest : MonoBehaviour
+{
+    private DialogueManager dialogueManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}
