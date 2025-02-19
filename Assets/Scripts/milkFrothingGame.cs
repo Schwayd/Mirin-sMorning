9778c654bb7bc1906962d3c9125b48f0cce556ca
Schwayd <Zschack@hotmail.com>
2025-02-19 00:38:14 +0000

    Milk frothing minigame
    You can now drag the jug up and down. There is an indicator on the left side which will turn green and yellow based on how close you are to the target slider when dragging the jug. If you get too far it wil turn red and spoil the milk.

---
+++ Assets/Scripts/milkFrothingGame.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class milkFrothingGame : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    [Header("UI Elements")]
+    [SerializeField] private RectTransform jug; //jug will be draggable
+    [SerializeField] private Slider targetSlider; //The slider that will move randomly
+    [SerializeField] private Image milkVisualCue; //Image that changes color as a warning
+
+    [Header("Settings")]
+    [SerializeField] private float badMilkThreshold = 50f; //Max distance allowed before the milk will start to spoil
+    [SerializeField] private float warningThreshold = 30f; //Distance where the cue starts to tell player they are close to spoiling
+    [SerializeField] private float targetMoveSpeed = 0.5f; //Max speed that the slider can move
+
+    private float jugStartY;
+    private bool isDragging = false;
+
+    void Start()
+    {
+        //Remembers the Y postiion of the jug initially
+        jugStartY = jug.anchoredPosition.y;
+
+        //Start the slider movement
+        StartCoroutine(MoveSliderRandomly());
+
+        //Resets the visual cue 
+        milkVisualCue.color = Color.white;
+    }
+
+    void Update()
+    {
+        CheckMilkStatus();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDragging = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Move the jug in Y axis based on where the mouse is
+        Vector2 newPosition = jug.anchoredPosition;
+        newPosition.y += eventData.delta.y;
+        jug.anchoredPosition = newPosition;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDragging = false;
+    }
+
+    private IEnumerator MoveSliderRandomly()
+    {
+        while (true)
+        {
+            //generates a new random Y position for the slider on the right
+            float targetY = Random.Range(-100f, 100f); //this is what can be adjusted to fit the UI
+            float startY = targetSlider.GetComponent<RectTransform>().anchoredPosition.y;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < 2f) // move over 2 seconds
+            {
+                float newY = Mathf.Lerp(startY, targetY, elapsedTime / 2f);
+                Vector2 newPos = targetSlider.GetComponent<RectTransform>().anchoredPosition;
+                newPos.y = newY;
+                targetSlider.GetComponent<RectTransform>().anchoredPosition = newPos;
+
+                elapsedTime += Time.deltaTime * targetMoveSpeed;
+                yield return null;
+
+
+
+            }
+
+
+        }
+
+
+
+    }
+
+    private void CheckMilkStatus()
+    {
+        //Calculte the distance between the jug and the slider on the right
+
+        float distance = Mathf.Abs(jug.anchoredPosition.y - targetSlider.GetComponent<RectTransform>().anchoredPosition.y);
+
+        if (distance > badMilkThreshold)
+        {
+            //Player failed and milk has gone bad
+            Debug.Log("Milk spoilt");
+            milkVisualCue.color = Color.red; //Turn indicator red
+        }
+        else if(distance > warningThreshold)
+        {
+            //Show a warning that milk is getting spoilt
+            milkVisualCue.color = Color.yellow;
+        }
+
+        else
+        {
+            //Resets the colour back to normal
+            milkVisualCue.color = Color.green;
+        }
+
+
+    }
+
+}
